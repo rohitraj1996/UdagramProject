f0ca466beb1c76b877e6e29bb37d1c82a3fdbf59
Rohit Raj <z.rohitraj1996@gmail.com>
2020-05-09 09:20:17 +0530

    Added code in @TODO section in server.ts, update package.json, package-lock.json for valid-url.

--- src/server.ts
+++ src/server.ts
@@ -9,23 +9,30 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
+
+  // Get valid-url object
+  // Credit: https://stackoverflow.com/questions/30931079/validating-a-url-in-node-js/30931191
+  var validUrl = require('valid-url');
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get("/filteredimage?:image_url", async( req, res ) => {
+    const { image_url } = req.query;
+
+    if (validUrl.isUri(image_url)) {
+      filterImageFromURL(image_url).then(response => {
+        res.status(200).sendFile(response);
+        res.on('finish', ()=>{
+          deleteLocalFiles([response]);
+        });
+      })
+
+    }
+    else
+      res.status(400).send("URL for Image is invalid.")
+  });
 
   /**************************************************************************** */
 
